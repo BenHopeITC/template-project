174ea3052c2dc4f13473b5cbb6c23587aee57ee6
Ben Hope <benhope.itc@gmail.com>
2024-09-15 10:07:14 +0100

    feature(BH-6): update jest to output lcov report needed by sonar

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
 	testEnvironment: 'node',
 	moduleDirectories: ['node_modules', '<rootDir>'],
 	testRegex: ['src/.*.test.js'],
+	collectCoverage: true,
+	coverageReporters: ['lcov', 'text'],
+	coverageDirectory: 'coverage',
 	collectCoverageFrom: ['src/**/*.js']
 };
