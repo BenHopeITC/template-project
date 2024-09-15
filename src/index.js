9bfa28b91a199f4cc8d05e5909eafe248ab4982e
Ben Hope <benhope.itc@gmail.com>
2024-09-15 09:50:12 +0100

    feature(BH-5): add tests

--- src/index.js
+++ src/index.js
@@ -1,33 +1,41 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
-const Logger = require('./lib/logger');
 
-const init = async () => {
-	const server = Hapi.server({
-		port: 3000,
-		host: 'localhost'
-	});
+const server = Hapi.server({
+	port: 3000,
+	host: 'localhost'
+});
 
-	server.route({
-		method: 'GET',
-		path: '/',
-		handler: (request, h) => {
-			console.log(`CONSOLE: Server received call using '${request.method}' method. Sonar shouldn't like this!`);
-			return 'Tutorial: Getting Started 14!';
-		}
-	});
+server.route({
+	method: 'GET',
+	path: '/',
+	handler: (request, h) => {
+		console.log(`CONSOLE: Server received call using '${request.method}' method. Sonar shouldn't like this!`);
+		return h.response('Tutorial: Testing! DO: npm run test');
+	}
+});
 
+server.route({
+	method: 'GET',
+	path: '/fail',
+	handler: (request, h) => {
+		return h.response({ message: 'Internal Server Error' }).code(500);
+	}
+});
+
+exports.init = async () => {
+	await server.initialize();
+	return server;
+};
+
+exports.start = async () => {
 	await server.start();
-	// debugger
-	// console.log('CONSOLE: Server running on %s', server.info.uri)
-	Logger().info('LOGGER: Server running on %s', server.info.uri);
-	Logger().trace('LOGGER TRACE: Server running on %s', server.info.uri);
+	console.log(`Server running at: ${server.info.uri}`);
+	return server;
 };
 
 process.on('unhandledRejection', (err) => {
 	console.log(err);
 	process.exit(1);
 });
-
-init();
